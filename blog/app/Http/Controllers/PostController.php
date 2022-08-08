062f2db6be34e40da8e5c6a222f20ef30265a5d9
yuudai01 <tremolo0331@gmail.com>
2022-08-08 18:51:36 +0900

    [add] 記事詳細画面

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    
     public function index(Post $post)
     {
-        return $post->get();
+    return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    } 
+    /**
+    * 特定IDのpostを表示する
+    *
+    * @params Object Post // 引数の$postはid=1のPostインスタンス
+    * @return Reposnse post view
+    */
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
     }
 }
+?>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,4 +11,8 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/{post}', 'PostController@show');
+
+?>
