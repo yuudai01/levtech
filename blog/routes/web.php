a6bec8294485966141e5ce88b61216f01783af1a
yuudai01 <tremolo0331@gmail.com>
2022-08-10 17:23:25 +0900

    [add]記事内容編集

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,5 +14,7 @@ Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::post('/posts', 'PostController@store');
 Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
 
 ?>
